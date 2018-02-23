6e853460a3f0c7330730a4f6a10aa9cc77a353c4
fjblau <fjblau@gmail.com>
2018-02-23 23:49:18 +0100

    2nd Part of 1st Tutorial

--- src/main/java/KeyGenerator.java
+++ src/main/java/KeyGenerator.java
@@ -1,5 +1,26 @@
+import org.nem.core.crypto.KeyPair;
+import org.nem.core.model.Address;
+import org.nem.core.model.NetworkInfos;
+
 public class KeyGenerator {
     public static void main(String[] args) {
-        System.out.println("Hello Frank");
+        while (true) {
+            final KeyPair someKey = new KeyPair();
+            final Address anAddress = Address.fromPublicKey(
+                    NetworkInfos.getTestNetworkInfo().getVersion(),
+                    someKey.getPublicKey());
+
+            if (anAddress.getEncoded().contains("TEST")) {
+                printKey(someKey, anAddress);
+                break;
+            }
+        }
+    }
+
+    
+    private static void printKey(KeyPair someKey, Address anAddress) {
+        System.out.println(String.format("Private key: %s", someKey.getPrivateKey()));
+        System.out.println(String.format(" Public key: %s", someKey.getPublicKey()));
+        System.out.println(String.format("    Address: %s", anAddress));
     }
 }
